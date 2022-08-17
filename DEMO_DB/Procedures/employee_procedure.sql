960ef469468f5302e18f6cea11104cce14900628
Srinath <srinath204@gmail.com>
2022-08-17 05:44:11 +0000

    employee_procedure.sql edited online with Bitbucket

--- DEMO_DB/Procedures/employee_procedure.sql
+++ DEMO_DB/Procedures/employee_procedure.sql
@@ -4,7 +4,6 @@ create or replace procedure demo_db.dqcoe_sample_proc (master1_id varchar)
 as 
 master_1 nvarchar2(100);
 BEGIN
-    select branch_id into master_1 from demo_db.branch where branch_id = master1_id
-        DBMS_OUTPUT.put_line ('master : '||master_1);
-
+    select branch_id into master_1 from demo_db.branch where branch_id = master1_id;
 end;
+/

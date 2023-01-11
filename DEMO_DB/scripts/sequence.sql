9a8a8b608e148ff0aa8390c326e1b189223cd728
Srinath <srinath204@gmail.com>
2023-01-11 05:13:19 +0000

    sequence.sql created online with Bitbucket

---
+++ DEMO_DB/scripts/sequence.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset srinath:create_branch_table endDelimiter:/
+CREATE SEQUENCE "DEMO_DB"."SEQ1" 
+START WITH 1
+MAXVALUE 9999999999999999999999999999 
+MINVALUE 1 
+INCREMENT BY 1  
+CACHE 20
+NOORDER CYCLE 
+NOPARTITION
+/

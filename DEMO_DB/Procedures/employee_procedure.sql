aab446883facb8f1f5a88db6df0f0399f47d697f
Srinath <srinath204@gmail.com>
2023-02-08 11:20:53 +0000

    employee_procedure.sql edited online with Bitbucket

--- DEMO_DB/Procedures/employee_procedure.sql
+++ DEMO_DB/Procedures/employee_procedure.sql
@@ -5,5 +5,8 @@ as
 master_1 nvarchar2(100);
 BEGIN
     select branch_id into master_1 from demo_db.branch where branch_id = master1_id;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+    dbms_output.put_line('No such branch!'); 
 end;
 /

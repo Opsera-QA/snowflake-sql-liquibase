732eb9ef7ca2da0681ffe158a18e48ad0acfb3ba
ramya jayaram <ramya.jayaram@opsera.io>
2023-02-09 09:42:50 +0000

    employee_create.sql edited online with Bitbucket

--- DEMO_DB/Tables/employee_create.sql
+++ DEMO_DB/Tables/employee_create.sql
@@ -3,10 +3,7 @@
 CREATE TABLE DEMO_DB.employee (
   emp_id INT PRIMARY KEY,
   first_name VARCHAR(40),
-  last_name VARCHAR(40),
-  birth_day DATE,
-  sex VARCHAR(1),
-  super_id INT,
-  branch_id INT
+   sex VARCHAR(1),
+   branch_id INT
 );
 --rollback DROP TABLE DEMO_DB.employee

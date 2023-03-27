1968531aa8b9cd12da492af7c21b9d5bf0024b00
ramya jayaram <ramya.jayaram@opsera.io>
2023-03-27 08:21:25 +0000

    branch_create.sql edited online with Bitbucket

--- DEMO_DB/Tables/branch_create.sql
+++ DEMO_DB/Tables/branch_create.sql
@@ -2,7 +2,7 @@
 --changeset srinath:create_branch_table
 CREATE TABLE DEMO_DB.branch (
 branch_id INT PRIMARY KEY,
-branch_name VARCHAR(40),
+branch_name VARCHAR(30),
 mgr_id INT,mgr_start_date DATE,
 FOREIGN KEY(mgr_id) REFERENCES demo_db.employee(emp_id) ON DELETE SET NULL);
 --rollback DROP TABLE DEMO_DB.branch

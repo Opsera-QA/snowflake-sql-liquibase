c9b89b727d32758f4dd480d9a9a3ea4c3ca12e5b
ramya jayaram <ramya.jayaram@opsera.io>
2023-03-27 08:43:03 +0000

    branch.sql edited online with Bitbucket

--- DEMO_DB/Views/branch.sql
+++ DEMO_DB/Views/branch.sql
@@ -1,10 +1,10 @@
 --liquibase formatted sql
 --changeset srinath:create_branch_view runAlways:true runOnChange:true runWith:sqlplus
-CREATE OR REPLACE VIEW DEMO_DB.v_branch 
+CREATE OR REPLACE VIEW DEMO_DB.v_branch1 
 AS
 SELECT 
   branch_id
-  FROM DEMO_DB.branch;
+  FROM DEMO_DB.branch1;
   
   EXEC demo_db.dqcoe_sample_proc ('100'); 
---rollback DROP VIEW DEMO_DB.v_branch
+--rollback DROP VIEW DEMO_DB.v_branch1

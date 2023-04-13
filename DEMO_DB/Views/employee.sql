8d3ea397382ab8293b270e6e7d36f954f1a551ee
Srinath <srinath204@gmail.com>
2023-04-13 06:51:19 +0000

    employee.sql edited online with Bitbucket

--- DEMO_DB/Views/employee.sql
+++ DEMO_DB/Views/employee.sql
@@ -2,12 +2,11 @@
 --changeset srinath:create_employee_view runOnChange:true runwith:sqlplus
 SET SERVEROUTPUT ON;
 SET DEFINE OFF;
-
 declare
     l_today date := sysdate;
 begin
     dbms_output.put_line(
-           'today is '||to_char(l_today,'Day'));
+           'today is - '||to_char(l_today,'Day'));
 exception when others then
     dbms_output.put_line(sqlerrm);
 end;
